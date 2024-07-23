396e85801abc7110f7c7764bac0f1b20ef72d71e
Icaro Almeida <icaro.almeida@v8.tech>
2024-07-23 19:10:59 -0300

    add commit

--- sql/ajustando_base.sql
+++ sql/ajustando_base.sql
@@ -19,10 +19,40 @@ end;
 
 -- Descobri que a MergeAluno não estão com todos os dados, por isso criei uma view mais completa, mais focada para ver a questão da inativação, 
 -- mas pode ser melhorada adicionando as novas colunas
-create view vw_aluno  as
+
+create or replace view vw_aluno  as
 select  t."IdAluno", t."IdUnidade", t."Sexo" , t."EstadoCivil" , t."DataNascimento" , t."CorRaca" , t."EnsinoMedio_IdEstabelecimentoEnsino" , 
 t."EnsinoMedio_AnoConclusao" , t2."IdTurma" , t2."IdSituacaoAlunoTurma" , t2."DataSituacaoAtivo" , t2."DataSituacaoInativo" , t2."DataHoraEfetivacaoMatricula", 
 t2."ProblemaAutorizadoMatricula" , t2."IdMotivoInativacao" , t2."ComentarioInativacao" , t2."IdPlanoPagamento_Matricula" , t3."MotivoInativacao" 
-from magic_steps.tbaluno t left join
-magic_steps.tbalunoturma t2 on t."IdAluno" = t2."IdAluno" 
-left join magic_steps.tbmotivoinativacao t3 on t2."IdMotivoInativacao" = t3."IdMotivoInativacao" ;
+from magic_steps.tbaluno t 
+FULL OUTER JOIN magic_steps.tbalunoturma t2 on t."IdAluno" = t2."IdAluno" 
+FULL OUTER JOIN magic_steps.tbmotivoinativacao t3 on t2."IdMotivoInativacao" = t3."IdMotivoInativacao" ;
+
+
+select * from magic_steps.tbalunoobs t limit 100;
+
+create or replace view magic_steps.vw_aluno_inativo as
+select va.*, obs."DataOcorrencia", obs."DataInclusao", obs."ObservacaoLiberacao", oc."NomeTipoOcorrencia", oc."IdTipoOcorrencia" from vw_aluno va 
+left join magic_steps.tbalunoobs obs on va."IdAluno" = obs."IdAluno" 
+left join magic_steps.tbtipoocorrencia oc  on obs."IdTipoOcorrencia" = oc."IdTipoOcorrencia" 
+where va."IdMotivoInativacao" is not null;
+
+
+select  * from tbtipoocorrencia t ;
+--31	Psicol da família: Atividade Passos em família
+
+select * from magic_steps.vw_aluno_inativo limit 100;
+
+
+select distinct "NomeTipoOcorrencia" from magic_steps.vw_aluno_inativo limit 100;
+
+select * from magic_steps.vw_aluno_inativo 
+where "IdTipoOcorrencia" = 31
+limit 100;
+
+
+---
+
+select count(distinct "IdAluno" ) from vw_aluno va where "IdMotivoInativacao" is null;
+
+select count(distinct "IdAluno" ) from vw_aluno_inativo vai; 
