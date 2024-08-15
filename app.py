c8f4e4ac3ad12a0638d9d6da124e2d2ccd0702ef
Icaro Almeida <icaro.almeida@v8.tech>
2024-08-15 00:40:58 -0300

    ajustes

--- app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Previsão de Evasão de Alunos",
-                   page_icon=":bar_chart:")
-
-st.title("Página Principal")
-st.write("Essa é a página principal do aplicativo.")
